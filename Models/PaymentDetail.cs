f2c7fd3c6ddece32009bc6b405e97e2769864e41
Samuel Tijani <omalitijani@gmail.com>
2020-09-05 16:55:44 +0100

    Define as required

--- Models/PaymentDetail.cs
+++ Models/PaymentDetail.cs
@@ -6,9 +6,13 @@ namespace test_mvc_webapp.Models
     {
         [Key]
         public int PMId { get; set; }
+        [Required]
         public string CardOwnerName { get; set; }
+        [Required]
         public string CardNumber { get; set; }
+        [Required]
         public string ExpirationDate { get; set; }
+        [Required]
         public string CVV { get; set; }
     }
 }
