80b4ed8753ba767bd9e61f0a9a00e473f840d54e
Samuel Tijani <omalitijani@gmail.com>
2020-09-05 17:06:06 +0100

    Set database datatypes

--- Models/PaymentDetail.cs
+++ Models/PaymentDetail.cs
@@ -1,4 +1,5 @@
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace test_mvc_webapp.Models
 {
@@ -7,12 +8,17 @@ namespace test_mvc_webapp.Models
         [Key]
         public int PMId { get; set; }
         [Required]
+        [Column(TypeName = "mvarchar(100)")]
         public string CardOwnerName { get; set; }
+        
         [Required]
+        [Column(TypeName = "varchar(16)")]
         public string CardNumber { get; set; }
         [Required]
+        [Column(TypeName = "varchar(5)")]
         public string ExpirationDate { get; set; }
         [Required]
+        [Column(TypeName = "varchar(3)")]
         public string CVV { get; set; }
     }
 }
