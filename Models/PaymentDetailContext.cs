ad9e64cec8fd646488d9b6e00fed2ba12812ba7c
Samuel Tijani <omalitijani@gmail.com>
2020-09-05 16:39:44 +0100

    Create a class

---
+++ Models/PaymentDetailContext.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace test_mvc_webapp.Models
+{
+    public class PaymentDetailContext
+    {
+        
+    }
+}
