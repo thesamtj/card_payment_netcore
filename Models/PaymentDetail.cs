4ebec856200cb6801e52bdd173e58eebfb865b36
Samuel Tijani <omalitijani@gmail.com>
2020-09-05 16:51:53 +0100

    Define properties

--- Models/PaymentDetail.cs
+++ Models/PaymentDetail.cs
@@ -2,6 +2,10 @@ namespace test_mvc_webapp.Models
 {
     public class PaymentDetail
     {
-        
+        public int PMId { get; set; }
+        public string CardOwnerName { get; set; }
+        public string CardNumber { get; set; }
+        public string ExpirationDate { get; set; }
+        public string CVV { get; set; }
     }
 }
