f72f0a482c2dd92c785b55e352b58e7137158851
Samuel Tijani <omalitijani@gmail.com>
2020-09-05 16:53:52 +0100

    Set key

--- Models/PaymentDetail.cs
+++ Models/PaymentDetail.cs
@@ -1,7 +1,10 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace test_mvc_webapp.Models
 {
     public class PaymentDetail
     {
+        [Key]
         public int PMId { get; set; }
         public string CardOwnerName { get; set; }
         public string CardNumber { get; set; }
