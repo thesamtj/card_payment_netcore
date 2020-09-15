de356b35e1ce6def0da5c66967aa230ca76c05df
Samuel Tijani <omalitijani@gmail.com>
2020-09-15 14:19:04 +0100

    Define properties

--- CardPaymentAPI/Models/PaymentDetail.cs
+++ CardPaymentAPI/Models/PaymentDetail.cs
@@ -7,6 +7,10 @@ namespace CardPaymentAPI.Models
 {
     public class PaymentDetail
     {
-
+        public int PMId { get; set; }
+        public string CardOwnerName { get; set; }
+        public string CardNumber { get; set; }
+        public string ExpirationDate { get; set; }
+        public string CVV { get; set; }
     }
 }
