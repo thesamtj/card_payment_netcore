e5cb25d6a5d1111a0c796bc7a5f91ff399a921e0
Samuel Tijani <omalitijani@gmail.com>
2020-09-15 14:04:34 +0100

    Add DbContext

--- CardPaymentAPI/Models/PaymentDetailContext.cs
+++ CardPaymentAPI/Models/PaymentDetailContext.cs
@@ -1,11 +1,12 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace CardPaymentAPI.Models
 {
-    public class PaymentDetailContext
+    public class PaymentDetailContext:DbContext
     {
     }
 }
