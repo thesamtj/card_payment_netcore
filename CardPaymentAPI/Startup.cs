a328348f1fa3cf22411db8cc950356d3f17a6e7a
Samuel Tijani <omalitijani@gmail.com>
2020-09-16 09:46:14 +0100

    Enable cors

--- CardPaymentAPI/Startup.cs
+++ CardPaymentAPI/Startup.cs
@@ -38,6 +38,8 @@ namespace CardPaymentAPI
             services.AddDbContext<PaymentDetailContext>(options =>
             options.UseSqlServer(Configuration.GetConnectionString("DevConnection"))
             );
+
+            services.AddCors();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -48,6 +50,11 @@ namespace CardPaymentAPI
                 app.UseDeveloperExceptionPage();
             }
 
+            app.UseCors(options =>
+            options.WithOrigins("http://localhost:4200")
+            .AllowAnyMethod()
+            .AllowAnyHeader()
+            );
             app.UseMvc();
         }
     }
